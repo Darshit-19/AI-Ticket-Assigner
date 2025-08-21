14e7cb68c69f9867d6e19fe8db41c3a91b0f1a6a
Darshit Hairiya <hairiyadarshit@gmail.com>
2025-08-21 14:41:42 +0530

    event key added

--- backend/inngest/client.js
+++ backend/inngest/client.js
@@ -3,4 +3,5 @@ import { Inngest } from "inngest";
 export const inngest = new Inngest({
   id: "ticketing-system",
   signingKey: process.env.INNGEST_SIGNING_KEY,
+  eventKey: process.env.INNGEST_EVENT_KEY
 });
